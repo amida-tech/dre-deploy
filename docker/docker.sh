56fd0ddd1ac632c776a4eccb17414c240b636eff
Jacob Sachs <jacob.s.sachs@gmail.com>
2015-12-04 13:19:23 -0500

    add proper URL_PATH for FHIR

--- docker/docker.sh
+++ docker/docker.sh
@@ -2,7 +2,7 @@ docker run --name dre-mongo -d mongo
 
 docker run --name dre-redis -d redis
 
-docker run -p 8080:8080 --name hapi-fhir -d amidatech/hapi-fhir
+docker run -p 8080:8080 --name hapi-fhir -e "URL_PATH=fhir" -d amidatech/hapi-fhir
 
 docker run -p 3000:3000 --name dre-services --link dre-mongo:mongo --link dre-redis:redis --link hapi-fhir:hapi  -d amidatech/dre-services
 
